92d17f3d02c0963fb0a02f336a213be041efd4b6
nbn2000 <makhmudov4744@list.ru>
2023-12-18 21:00:43 +0500

    gallary section is added

--- src/sections/Home/view/SectionHome.tsx
+++ src/sections/Home/view/SectionHome.tsx
@@ -1,12 +1,14 @@
 import Header from "../Header";
 import NewProducts from "../NewProducts";
 import WhatWeOffer from "../WhatWeOffer";
+import Gallery from "../Gallery";
 const Home = () => {
   return (
     <div>
       <Header />
       <WhatWeOffer />
       <NewProducts />
+      <Gallery />
     </div>
   );
 };
