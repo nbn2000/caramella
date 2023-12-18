ff71657706336b7d0a191eabb0d3966cf6f6bdd4
nbn2000 <makhmudov4744@list.ru>
2023-12-18 21:10:20 +0500

    link added to main image in gallary section

--- src/sections/Home/Gallery.tsx
+++ src/sections/Home/Gallery.tsx
@@ -6,22 +6,27 @@ import cupCake from "@/assets/gallery/cup-cakes.jpg";
 import customCake from "@/assets/gallery/custom-cakes.jpg";
 import macarons from "@/assets/gallery/macarons.jpg";
 import weddingCake from "@/assets/gallery/wedding-cake.jpg";
+import Link from "next/link";
 import { useState } from "react";
 
 const Gallery = () => {
-  const [mainImg, setMainImg] = useState({ id: 1, img: birthdayCake });
+  const [mainImg, setMainImg] = useState({
+    id: 1,
+    img: birthdayCake,
+    link: "/menu/birthday-cake",
+  });
   const [transition, setTransition] = useState(false);
   const hovered =
     "rounded-[50%] w-full h-full transition-[opacity] duration-200 ease-in-out";
   const unHovered =
     "rounded-[50%] opacity-[0.6] w-full h-full transition-[opacity] duration-200 ease-in-out";
 
-  const handleHover = (a: number, b: any) => {
-    if (mainImg.id !== a) {
+  const handleHover = (id: number, img: any, link: string) => {
+    if (mainImg.id !== id) {
       setTransition(true);
       const timeout = setTimeout(() => {
         setTransition(false);
-        setMainImg({ id: a, img: b });
+        setMainImg({ id: id, img: img, link: link });
         clearTimeout(timeout);
       }, 200);
     } else {
@@ -37,7 +42,10 @@ const Gallery = () => {
         </span>
       </h2>
       <div className="max-w-full mx-auto flex justify-center items-center relative">
-        <div className="aspect-square w-[30%] my-[6rem] lg:w-[40%]">
+        <Link
+          href={mainImg.link}
+          className="aspect-square w-[30%] my-[6rem] lg:w-[40%]"
+        >
           <Image
             src={mainImg.img}
             alt="hovered image"
@@ -47,15 +55,15 @@ const Gallery = () => {
               transition ? "opacity-0" : "opacity-100"
             }`}
           />
-        </div>
+        </Link>
         <div
           className="wedding-cake absolute top-[0%] right-[25%] aspect-square w-[9%] lg:w-[13%] lg:right-[15%]"
-          onMouseMove={() => handleHover(1, weddingCake)}
+          onMouseMove={() => handleHover(1, weddingCake, "/menu/wedding-cake")}
         >
           <div className="relative w-full h-full">
             <Image
               src={weddingCake}
-              alt="birthday cake"
+              alt="wedding cake"
               width={80}
               height={80}
               className={mainImg.id === 1 ? hovered : unHovered}
@@ -68,12 +76,12 @@ const Gallery = () => {
         </div>
         <div
           className="biscuit absolute right-[25%] bottom-[0%] aspect-square w-[9%] lg:w-[13%] lg:right-[15%]"
-          onMouseMove={() => handleHover(2, biscuits)}
+          onMouseMove={() => handleHover(2, biscuits, "/menu/biscuits")}
         >
           <div className="relative w-full h-full">
             <Image
               src={biscuits}
-              alt="birthday cake"
+              alt="biscuits"
               width={80}
               height={80}
               className={mainImg.id === 2 ? hovered : unHovered}
@@ -85,12 +93,12 @@ const Gallery = () => {
         </div>
         <div
           className="cupcake absolute left-[15%] top-[50%] -translate-y-[50%] aspect-square w-[9%] lg:w-[13%] lg:left-[5%]"
-          onMouseMove={() => handleHover(3, cupCake)}
+          onMouseMove={() => handleHover(3, cupCake, "/menu/cupcake")}
         >
           <div className="relative w-full h-full">
             <Image
               src={cupCake}
-              alt="birthday cake"
+              alt="cup cake"
               width={80}
               height={80}
               className={mainImg.id === 3 ? hovered : unHovered}
@@ -102,12 +110,12 @@ const Gallery = () => {
         </div>
         <div
           className="custom-cake absolute left-[25%] bottom-[0%] aspect-square w-[9%] lg:w-[13%] lg:left-[15%]"
-          onMouseMove={() => handleHover(4, customCake)}
+          onMouseMove={() => handleHover(4, customCake, "/menu/custom-cake")}
         >
           <div className="relative w-full h-full">
             <Image
               src={customCake}
-              alt="birthday cake"
+              alt="custom cake"
               width={80}
               height={80}
               className={mainImg.id === 4 ? hovered : unHovered}
@@ -120,12 +128,12 @@ const Gallery = () => {
 
         <div
           className="macarons absolute right-[15%] top-[50%] -translate-y-[50%] aspect-square w-[9%] lg:w-[13%] lg:right-[5%]"
-          onMouseMove={() => handleHover(5, macarons)}
+          onMouseMove={() => handleHover(5, macarons, "/menu/macarons")}
         >
           <div className="relative w-full h-full">
             <Image
               src={macarons}
-              alt="birthday cake"
+              alt="macarons"
               width={80}
               height={80}
               className={mainImg.id === 5 ? hovered : unHovered}
@@ -137,7 +145,9 @@ const Gallery = () => {
         </div>
         <div
           className="birthday-cake absolute left-[25%] top-[0%] lg:left-[15%]  aspect-square w-[9%] lg:w-[13%]"
-          onMouseMove={() => handleHover(6, birthdayCake)}
+          onMouseMove={() =>
+            handleHover(6, birthdayCake, "/menu/birthday-cake")
+          }
         >
           <div className="relative w-full h-full ">
             <Image
